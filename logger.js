2378d60e11727395dc96b188725dec1773a55102
Mikey <109159459+MiDev7@users.noreply.github.com>
2024-10-09 19:56:33 +0400

    Fix: Fix logger

--- logger.js
+++ logger.js
@@ -1,6 +1,7 @@
 function logger(res, req, next) {
   let currentTime = new Date().toLocaleString();
-  console.log(`[${currentTime}] ${req.method} ${req.path}`);
+  console.log(`[${currentTime}] ${req.req.method} ${req.req.url} `);
+  next();
 }
 
 export default logger;
