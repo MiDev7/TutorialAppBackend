6614d528e13f586fc35d5b8ce520a180580a3be2
Mikey <109159459+MiDev7@users.noreply.github.com>
2024-11-18 21:27:16 +0400

    Feat: Remove logger js

--- logger.js
+++
@@ -1,7 +0,0 @@
-function logger(res, req, next) {
-  let currentTime = new Date().toLocaleString();
-  console.log(`[${currentTime}] ${req.req.method} ${req.req.url} `);
-  next();
-}
-
-export default logger;
